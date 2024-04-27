628bb172f6d5357fdc46f5c49a74d5e359068805
Ji__ho <cale.jiho@gmail.com>
2024-04-27 22:16:18 +0900

    ✨ Feat: Background color
    main page background color added

--- header.php
+++ header.php
@@ -120,6 +120,16 @@
                                         <li class="sub"><a href="/archive/2023_graduate/about/Faculty.html">지도교수 <small>Faculty Members</small></a></li>
                                     </ul>
                                 </div>
+                                <div class="sub_07">
+                                    <i></i>
+                                    <a class="over" href="/performance_main.html">2024 작품발표회</a>
+                                    <ul class="menu_sub_07">
+                                        <i></i>
+                                        <li class="sub"><a href="/about/Congratulatory.html">축사 <small>Congratulatory Message</small></a></li>
+                                        <li class="sub"><a href="/about/Greetings.html">인사말 <small>Greetings</small></a></li>
+                                        <li class="sub"><a href="/about/Faculty.html">지도교수 <small>Faculty Members</small></a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </li>
                         <li class="hover_menu only_04">
@@ -270,6 +280,20 @@
                                                 <li class="sub"><a href="/archive/2023_graduate/about/Congratulatory.html">축사 <small>Congratulatory Message</small></a></li>
                                                 <li class="sub"><a href="/archive/2023_graduate/about/Greetings.html">인사말 <small>Greetings</small></a></li>
                                                 <li class="sub"><a href="/archive/2023_graduate/about/Faculty.html">지도교수 <small>Faculty Members</small></a></li>
+                                            </ul>
+                                        </li>  
+                                        <li class="sub">
+                                            <a href="/performance_main.html">2024 작품발표회
+                                                <span>
+                                                    <a href="#" >
+                                                        <img src="/img/gnb_arrow.png" alt="서브메뉴오픈" width="12">
+                                                    </a>
+                                                </span>
+                                            </a>                                
+                                            <ul class="cont_m cont_m_01">
+                                                <li class="sub"><a href="/about/Congratulatory.html">축사 <small>Congratulatory Message</small></a></li>
+                                                <li class="sub"><a href="/about/Greetings.html">인사말 <small>Greetings</small></a></li>
+                                                <li class="sub"><a href="/about/Faculty.html">지도교수 <small>Faculty Members</small></a></li>
                                             </ul>
                                         </li>  
                                     </ul>
