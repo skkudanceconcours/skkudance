2ddf9a676a6ffc756559b475dd757ee42b9943c2
JiHo00 <cale.jiho@gmail.com>
2024-11-23 21:38:55 +0900

    2024 졸업발표회 보존 완료 / 쇼케이스 헤더 생성

--- alumni/2024/header.php
+++ alumni/2024/header.php
@@ -150,14 +150,25 @@
                                 </div>
                                 <div class="sub_08">
                                     <i></i>
-                                    <a class="over" href="/performance_main.html">2024 졸업발표회</a>
+                                    <a class="over" href="/archive/2024_graduate/index.html">2024 졸업발표회</a>
                                     <ul class="menu_sub_08">
+                                        <i></i>
+                                        <li class="sub"><a href="/archive/2024_graduate/about/Congratulatory.html">축사 <small>Congratulatory Message</small></a></li>
+                                        <li class="sub"><a href="/archive/2024_graduate/about/Greetings.html">인사말 <small>Greetings</small></a></li>
+                                        <li class="sub"><a href="/archive/2024_graduate/about/Faculty.html">지도교수 <small>Faculty Members</small></a></li>
+                                    </ul>
+                                </div>
+                                <div class="sub_09">
+                                    <i></i>
+                                    <a class="over" href="/performance_main.html">2024 쇼케이스</a>
+                                    <ul class="menu_sub_09">
                                         <i></i>
                                         <li class="sub"><a href="/about/Congratulatory.html">축사 <small>Congratulatory Message</small></a></li>
                                         <li class="sub"><a href="/about/Greetings.html">인사말 <small>Greetings</small></a></li>
                                         <li class="sub"><a href="/about/Faculty.html">지도교수 <small>Faculty Members</small></a></li>
                                     </ul>
                                 </div>
+                                
                             </div>
                         </li>
                         <li class="hover_menu only_04">
@@ -334,7 +345,21 @@
                                             </ul>
                                         </li> 
                                         <li class="sub">
-                                            <a href="/performance_main.html">2024 졸업발표회
+                                            <a href="/archive/2024_graduate/index.html">2024 졸업발표회
+                                                <span>
+                                                    <a href="#" >
+                                                        <img src="/img/gnb_arrow.png" alt="서브메뉴오픈" width="12">
+                                                    </a>
+                                                </span>
+                                            </a>                                
+                                            <ul class="cont_m cont_m_01">
+                                                <li class="sub"><a href="/archive/2024_graduate/about/Congratulatory.html">축사 <small>Congratulatory Message</small></a></li>
+                                                <li class="sub"><a href="/archive/2024_graduate/about/Greetings.html">인사말 <small>Greetings</small></a></li>
+                                                <li class="sub"><a href="/archive/2024_graduate/about/Faculty.html">지도교수 <small>Faculty Members</small></a></li>
+                                            </ul>
+                                        </li>  
+                                        <li class="sub">
+                                            <a href="/performance_main.html">2024 쇼케이스
                                                 <span>
                                                     <a href="#" >
                                                         <img src="/img/gnb_arrow.png" alt="서브메뉴오픈" width="12">
